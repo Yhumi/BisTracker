a3e655d800a00240565640d8a9a3c78c07ca21ad
Yhumi <yhumi@miyei.me>
2024-08-20 10:45:29 +0100

    Initial work on the MeldHandler

---
+++ BisTracker/Melding/MeldHandler.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisTracker.Melding
+{
+    internal static class MeldHandler
+    {
+        public static uint CurrentWorkingPiece = 0;
+        public static uint SelectedWorkingJob = 0;
+        public static uint SelectedWorkingBis = 0;
+
+        public static void SetupAutoMeld()
+        {
+
+        }
+    }
+}
